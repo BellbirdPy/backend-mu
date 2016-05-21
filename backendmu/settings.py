6664a788186d6369b5f73da15422efa7fd6d9f48
gmacchi93 <guillemacchi01@gmail.com>
2016-05-21 19:05:55 -0400

    mas settings para heroku

--- backendmu/settings.py
+++ backendmu/settings.py
@@ -101,12 +101,12 @@ WSGI_APPLICATION = 'backendmu.wsgi.application'
 
 DATABASES = {
  'default': {
- 'ENGINE': 'django.db.backends.postgresql_psycopg2',
- 'NAME': 'd9r50b1np87ngi',
- 'USER': 'kwbotkykupjpqc',
- 'PASSWORD': 'hrnQ1k3VVynjnd79dR5CBh8wmW',
- 'HOST': 'ec2-54-243-199-161.compute-1.amazonaws.com',
-     'PORT': 5432
+     'ENGINE': 'django.db.backends.postgresql',
+     'NAME': 'd9r50b1np87ngi',
+     'USER': 'kwbotkykupjpqc',
+     'PASSWORD': 'hrnQ1k3VVynjnd79dR5CBh8wmW',
+     'HOST': 'ec2-54-243-199-161.compute-1.amazonaws.com',
+     'PORT': '5432',
  }
 }
 
@@ -154,22 +154,12 @@ SERVER_EMAIL = 'gmacchi@bellbird.com.py'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = 'static'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
