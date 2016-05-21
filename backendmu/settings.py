f8fed4ca8dfd58b43b6a71e613eef8865196a781
gmacchi93 <guillemacchi01@gmail.com>
2016-05-21 18:42:49 -0400

    DATABASES modificado

--- backendmu/settings.py
+++ backendmu/settings.py
@@ -100,10 +100,14 @@ WSGI_APPLICATION = 'backendmu.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 'NAME': 'd9r50b1np87ngi',
+ 'USER': 'kwbotkykupjpqc',
+ 'PASSWORD': 'hrnQ1k3VVynjnd79dR5CBh8wmW',
+ 'HOST': 'ec2-54-243-199-161.compute-1.amazonaws.com',
+     'PORT': 5432
+ }
 }
 
 # Password validation
