da78086f11c8e433d5469d0f12157e5a339aca02
gmacchi93 <guillemacchi01@gmail.com>
2016-05-23 16:07:21 -0400

    sesettings cambiado para heroku

--- staticfiles/scripts/services/animal.js
+++ staticfiles/scripts/services/animal.js
@@ -9,7 +9,7 @@
  */
 angular.module('frontendmuApp')
   .factory('Animal', function ($resource) {
-    return $resource('http://localhost:8000/api/animal/:id/',null,{
+    return $resource('/api/animal/:id/',null,{
       'update': { method:'PUT' },
       'delete': {method:'DELETE'}
     });
