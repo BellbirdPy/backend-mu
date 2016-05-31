f2acb76b5c2e713df5e5a7dbd8752f92bcd246e6
gmacchi93 <guillemacchi01@gmail.com>
2016-05-31 11:40:40 -0400

    - se mejoro inventario

--- nutricion/models.py
+++ nutricion/models.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+
+from django.template.defaultfilters import register
+from django.utils import timezone
 from lote.models import *
 from django.db import models
 
@@ -15,3 +18,10 @@ class Nutricion(models.Model):
 
     def __unicode__(self):
         return unicode(self.tipo_nutricion)
+
+    def is_activo(self):
+        ahora = timezone.now()
+        if ahora >= self.fecha_inicio and ahora <= self.fecha_fin:
+            return True
+        else:
+            return False
--- nutricion/views.py
+++ nutricion/views.py
@@ -1,10 +1,13 @@
+from django_filters import MethodFilter
 from rest_framework import viewsets,filters
 from serializers import *
 from models import *
 # Create your views here.
-
+ahora = timezone.now()
 class NutricionViewSet(viewsets.ModelViewSet):
     serializer_class = NutricionSerializer
     queryset = Nutricion.objects.all()
-    filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('establecimiento',)
+    filter_backends = (filters.DjangoFilterBackend,filters.OrderingFilter)
+    filter_fields = ('establecimiento','lotes','tipo_nutricion',)
+    ordering_fields = '__all__'
+    ordering = ('fecha_inicio',)
