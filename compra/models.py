8d1542decd41e32f5cdce0963d467c6ab99c33d4
rauleb <rauleburro@gmail.com>
2016-07-05 19:24:39 -0400

    -Arreglamos todos los temas para la compra y sus detalles

--- compra/models.py
+++ compra/models.py
@@ -4,19 +4,22 @@ from configuracion.models import Categoria, Raza
 from django.db import models
 from establecimiento.models import Establecimiento
 
-
 # Create your models here.
+from lote.models import Lote
+
+
 class Compra(models.Model):
     establecimiento = models.ForeignKey(Establecimiento, related_name="compras")
     cod_establecimiento_vendedor = models.CharField(max_length=32)
     nombre_vendedor = models.CharField(max_length=100)
     numero_guia = models.IntegerField()
     fecha_compra = models.DateField()
+    precio_total = models.BigIntegerField(blank=True, null=True, default=0)
     fecha_creacion = models.DateField(auto_now=True)
 
     def __unicode__(self):
         return unicode(self.cod_establecimiento_vendedor + ' ' + self.nombre_vendedor + ' '
-                       + self.fecha.strftime('%d-%m-%Y'))
+                       + self.fecha_compra.strftime('%d-%m-%Y'))
 
 
 class DetalleCompra(models.Model):
@@ -27,6 +30,7 @@ class DetalleCompra(models.Model):
     compra = models.ForeignKey(Compra, related_name="detalle_compra", on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, null=True, on_delete=models.SET_NULL, related_name="detalle_compra")
     raza = models.ForeignKey(Raza, null=True, on_delete=models.SET_NULL, related_name="detalle_compra")
+    lote = models.ForeignKey(Lote, null=True, on_delete=models.SET_NULL, related_name="detalle_compra")
 
     def __unicode__(self):
         return unicode(self.categoria.nombre + ' ' + self.cantidad.__str__())
--- compra/serializers.py
+++ compra/serializers.py
@@ -1,8 +1,8 @@
 from animal.models import Animal
 from animal.serializers import AnimalSerializer
-from configuracion.serializers import CategoriaSerializer, RazaSerializer
-from rest_framework import serializers
+from lote.models import Lote
 from models import *
+from rest_framework import serializers
 
 
 class DetalleCompraSerialzer(serializers.ModelSerializer):
@@ -10,7 +10,7 @@ class DetalleCompraSerialzer(serializers.ModelSerializer):
 
     class Meta:
         model = DetalleCompra
-        fields = ['id', 'categoria', 'carimbo', 'raza', 'cantidad', 'caravana_inicial', 'animales']
+        fields = ['id', 'categoria', 'carimbo', 'raza', 'cantidad', 'caravana_inicial', 'lote', 'animales']
 
 
 class CompraSerializer(serializers.ModelSerializer):
@@ -18,7 +18,8 @@ class CompraSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Compra
-        fields = ['id', 'establecimiento', 'cod_establecimiento_vendedor', 'nombre_vendedor', 'fecha', 'detalle_compra']
+        fields = ['id', 'establecimiento', 'cod_establecimiento_vendedor', 'nombre_vendedor', 'numero_guia',
+                  'fecha_compra', 'precio_total', 'detalle_compra']
 
     def create(self, validated_data):
         detalle_data = validated_data.pop('detalle_compra')
@@ -30,6 +31,7 @@ class CompraSerializer(serializers.ModelSerializer):
                            carimbo=new_detalle_compra.carimbo,
                            categoria=Categoria.objects.get(id=new_detalle_compra.categoria_id),
                            raza=Raza.objects.get(id=new_detalle_compra.raza_id),
+                           lote=Lote.objects.get(id=new_detalle_compra.lote_id),
                            establecimiento=Establecimiento.objects.get(id=compra.establecimiento_id),
                            detalle_compra=new_detalle_compra)
                 a.save()
