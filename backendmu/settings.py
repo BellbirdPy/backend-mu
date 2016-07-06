fd24b35a393a420666292818d72ed03008ba8333
gmacchi93 <guillemacchi01@gmail.com>
2016-07-05 22:00:42 -0400

    cambios para local

--- backendmu/settings.py
+++ backendmu/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'backendmu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-'''
+
 DATABASES = {
  'default': {
      'ENGINE': 'django.db.backends.postgresql',
@@ -120,7 +120,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
