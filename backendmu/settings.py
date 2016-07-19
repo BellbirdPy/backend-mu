2fdfeff4b498959fa5275a2f1ab76635b45c5724
gmacchi <guillemacchi01@gmail.com>
2016-07-19 16:55:56 -0400

    - settings y gitignore actualizado

--- backendmu/settings.py
+++ backendmu/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'backendmu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+'''
 DATABASES = {
  'default': {
      'ENGINE': 'django.db.backends.postgresql',
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
