bc6b8e8ce266e3a59134c2b60ede2105d747ff9a
rauleb <rauleburro@gmail.com>
2016-08-16 11:23:48 -0400

    -ya funciona el script que hace que cuando se cargue una genetica de lote automaticamente se carguen algunos campos de los animales especificamente para su genetica

--- genetica/models.py
+++ genetica/models.py
@@ -7,7 +7,7 @@ from configuracion.models import Raza, Categoria
 from lote.models import Lote
 
 porcentaje = ((25, 25), (50, 50), (75, 75), (100, 100))
-t_servicio = (('MN','Monta Natural'), ('IA','Inceminacion Artificial'))
+t_servicio = (('MN', 'Monta Natural'), ('IA', 'Inceminacion Artificial'))
 car = ((0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9))
 
 
@@ -27,13 +27,13 @@ class LoteGenetica(models.Model):
 class AnimalGenetica(models.Model):
     establecimiento = models.ForeignKey(Establecimiento, related_name="animal_genetica")
     animal = models.OneToOneField(Animal)
-    nombre = models.CharField(max_length=128)
-    nombre_corto = models.CharField(max_length=32)
+    nombre = models.CharField(max_length=128,null=True,blank=True)
+    nombre_corto = models.CharField(max_length=32,null=True,blank=True)
     porcentaje_pureza = models.PositiveIntegerField(choices=porcentaje)
     tipo_servicio = models.CharField(max_length=2, choices=t_servicio)
-    rp = models.CharField(max_length=32)
-    descripcion = models.TextField()
-    cantidad_pajuelas = models.PositiveIntegerField()
-    pedigree_padre = models.CharField(max_length=32)
-    pedigree_madre = models.CharField(max_length=32)
-    pedigree_abuelo = models.CharField(max_length=32)
+    rp = models.CharField(max_length=32,null=True,blank=True)
+    descripcion = models.TextField(null=True,blank=True)
+    cantidad_pajuelas = models.PositiveIntegerField(default=0, null=True,blank=True)
+    pedigree_padre = models.CharField(max_length=32,null=True,blank=True)
+    pedigree_madre = models.CharField(max_length=32,null=True,blank=True)
+    pedigree_abuelo = models.CharField(max_length=32,null=True,blank=True)
--- genetica/serializers.py
+++ genetica/serializers.py
@@ -1,20 +1,36 @@
 from rest_framework import serializers
 
 from animal.serializers import AnimalSerializer
-from configuracion.serializers import RazaSerializer, CategoriaSerializer
-from lote.serializers import LoteSerializer
 from models import *
 
 
 class LoteGeneticaSerializer(serializers.ModelSerializer):
-    lote = LoteSerializer(many=False, read_only=True)
-    raza = RazaSerializer(many=False, read_only=True)
-    categoria = CategoriaSerializer(many=False, read_only=True)
+    lote_nombre = serializers.CharField(source='lote.nombre', read_only=True)
+    raza_nombre = serializers.CharField(source='raza.nombre', read_only=True)
+    categoria_nombre = serializers.CharField(source='categoria.nombre', read_only=True)
 
     class Meta:
         model = LoteGenetica
-        fields = ['id', 'establecimiento', 'lote', 'raza', 'categoria', 'porcentaje_pureza', 'tipo_servicio',
+        fields = ['id', 'establecimiento', 'lote', 'lote_nombre', 'raza', 'raza_nombre', 'categoria',
+                  'categoria_nombre', 'porcentaje_pureza', 'tipo_servicio',
                   'carimbo', 'descripcion', 'pedigree_padre']
+
+    def create(self, validated_data):
+        loteGenetica = LoteGenetica.objects.create(**validated_data)
+        lote = Lote.objects.get(id=loteGenetica.lote_id)
+        animales = Animal.objects.all().filter(lote=lote)
+        i = 0
+        for animal in animales:
+            a = AnimalGenetica(establecimiento=loteGenetica.establecimiento,
+                               animal=animal,
+                               porcentaje_pureza=loteGenetica.porcentaje_pureza,
+                               tipo_servicio=loteGenetica.tipo_servicio,
+                               descripcion=loteGenetica.descripcion,
+                               pedigree_padre=loteGenetica.pedigree_padre)
+            a.save()
+            i = i + 1
+            print i, 'Listo: animal ', animal.id
+        return loteGenetica
 
 
 class AnimalGeneticaSerializer(serializers.ModelSerializer):
