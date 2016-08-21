2f38f671623c86fbe4848851204138fd4cb633c9
rubenfig <mangekyou1>
2016-08-21 18:39:18 -0400

    -Se areglaron bugs y agrego la opcion de listar todos los tipos de egreso

--- contabilidad/views.py
+++ contabilidad/views.py
@@ -6,18 +6,65 @@ from serializers import *
 from models import *
 # Create your views here.
 class ReporteEgresoViewSet (viewsets.ViewSet):
+
+
     def list(self, request, format=None):
         if (request.query_params):
             mes = request.query_params.get('mes')
             anho = request.query_params.get('anho')
             establecimiento = request.query_params.get('establecimiento')
             reportes = []
-            rubros = [{'c':'GD','n':'Gastos Directos'}, {'c':'GA','n':'Gastos Administrativos'}, {'c':'IT','n':'Impuestos y Tazas'},
-                      {'c':'GC', 'n':'Gastos de Comercializacion'}, {'c':'GF','n':'Gastos Financieros'}]
+            rubros = [{'c':'GD','n':'Gastos Directos',
+                       'descripciones': ['Sueldos y Jornales',
+                                       'Manutención',
+                                       'Gastos Sociales',
+                                       'Bonificaciones',
+                                       'Sanidad Animal',
+                                       'Mineralización',
+                                       'Gastos de Reproducción (Servicios)',
+                                       'Suplementación Alimenticia',
+                                       'Combustibles y Lubricantes',
+                                       'Reparaciones y Mantemientos de Maquinarias y Equipos',
+                                       'Mantenimiento de Instalaciones',
+                                       'Servicios Públicos y Comunicación',
+                                       'Movilidad',
+                                       'Varios',
+                                       'Imprevistos']
+                       }, {'c':'GA','n':'Gastos Administrativos',
+                           'descripciones':[
+                                      'Honorarios del Administrador',
+                                      'Personal Administrativo',
+                                      'Alquileres',
+                                      'Servicios Públicos de Oficina',
+                                      'Cuotas Sociales',
+                                      'Asesoramiento Técnico',
+                                      'Asistencia Contable',
+                                      'Otros Gastos de Oficina',
+                                      'Imprevistos']},
+                      {'c':'IT','n':'Impuestos y Tazas',
+                       'descripciones': [
+                           'Inmobiliario',
+                           'Otros']
+                       },
+                      {'c':'GC', 'n':'Gastos de Comercialización',
+                       'descripciones': [
+                           'Guías de Translado y Control de Marcas',
+                           'Transporte',
+                           'Comisiones',
+                           'Otros']
+                       },
+                      {'c':'GF','n':'Gastos Financieros',
+                       'descripciones': [
+                           'Intereses sobre Préstamos',
+                           'Otros Financieros',
+                           'Pago de Capital de Préstamos']
+                       }]
             total_reporte = 0
             for rubro in rubros:
                 descripciones = []
                 total_rubro = 0
+                for descripcion in rubro['descripciones']:
+                    descripciones.append({'nombre': descripcion, 'monto': 0, 'porcentaje': 0.0})
                 for egreso in Egreso.objects.filter(establecimiento=establecimiento, rubro=rubro['c'], fecha__month=mes, fecha__year=anho)\
                         .values('descripcion').distinct():
                     total = 0
@@ -25,13 +72,18 @@ class ReporteEgresoViewSet (viewsets.ViewSet):
                                                              fecha__month=mes, fecha__year=anho)\
                             .values('monto'):
                         total = total + descripcion['monto']
-                    descripciones.append({'nombre': egreso['descripcion'], 'monto': total, 'porcentaje': 0.0})
-                    total_rubro = total_rubro + total
+                    if not any(d.get('nombre', None) == egreso['descripcion'] for d in descripciones):
+                        descripciones.append({'nombre': egreso['descripcion'], 'monto': total, 'porcentaje': 0.0})
+                    else:
+                        for d in descripciones:
+                            if (d['nombre']) == egreso['descripcion']:
+                                d['monto'] = total
+                    total_rubro += total
                     for des in descripciones:
                         print des['monto']/total_rubro
                         des['porcentaje'] = round((des['monto']/total_rubro)*100, 2)
                 reportes.append({'rubro': rubro['n'], 'monto_rubro': total_rubro, 'descripciones': descripciones})
-                total_reporte = total_reporte + total_rubro
+                total_reporte += total_rubro
             response = {'count': len(reportes), 'next': None, 'previous': None, 'results': reportes, 'total_reporte': total_reporte}
         else:
             response = {'count': 0, 'next': None, 'previous': None, 'results': None, 'total_reporte': None}
