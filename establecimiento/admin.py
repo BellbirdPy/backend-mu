87a05454dcc0fcfc47c1dd2142828660b3a4ad2d
gmacchi <guillemacchi01@gmail.com>
2016-10-30 17:58:53 -0300

    - actualizado admin para ver fecha creacion

--- establecimiento/admin.py
+++ establecimiento/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from models import *
 
 # Register your models here.
+class EstablecimientoAdmin(admin.ModelAdmin):
+    readonly_fields = ('fecha_creacion',)
 admin.site.register(Departamento)
 admin.site.register(Ciudad)
-admin.site.register(Establecimiento)
+admin.site.register(Establecimiento, EstablecimientoAdmin)
