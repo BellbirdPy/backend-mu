df22ead26056e04db8520471991dc4c5ee3946fe
gmacchi <guillemacchi01@gmail.com>
2016-11-27 17:24:16 -0300

    - cambios en el user admin

--- establecimiento/admin.py
+++ establecimiento/admin.py
@@ -4,6 +4,13 @@ from models import *
 # Register your models here.
 class EstablecimientoAdmin(admin.ModelAdmin):
     readonly_fields = ('fecha_creacion',)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','email','first_name','last_name','last_login')
+
 admin.site.register(Departamento)
 admin.site.register(Ciudad)
 admin.site.register(Establecimiento, EstablecimientoAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
