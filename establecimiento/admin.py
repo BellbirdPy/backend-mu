230f756456f66b9e937537bfd185534d0c7d5b28
gmacchi <guillemacchi01@gmail.com>
2016-11-27 17:51:29 -0300

    - cambios en el establecimiento admin

--- establecimiento/admin.py
+++ establecimiento/admin.py
@@ -4,6 +4,17 @@ from models import *
 # Register your models here.
 class EstablecimientoAdmin(admin.ModelAdmin):
     readonly_fields = ('fecha_creacion',)
+    list_display = ('nombre','fecha_creacion','get_owner','get_owner_mail')
+
+    def get_owner(self, obj):
+        return obj.owner.first_name + ' ' + obj.owner.last_name
+    get_owner.short_description = 'Dueno'
+
+    def get_owner_mail(self, obj):
+        return obj.owner.email
+
+    get_owner.short_description = 'Dueno email'
+
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username','email','first_name','last_name','last_login')
